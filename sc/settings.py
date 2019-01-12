ebc7f4179d768e92622386769230d8feaa044bff
taojy123 <taojy123@163.com>
2019-01-12 14:25:27 +0800

    set debug false

--- sc/settings.py
+++ sc/settings.py
@@ -7,7 +7,7 @@ import uuid
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 LOGIN_URL = '/login/'
 
